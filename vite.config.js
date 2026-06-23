7f9b2ecc62e5cd093919939049188e77575eaeb1
dhatchana004-cyber <dhatchana004@gmail.com>
2026-06-23 15:23:18 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -7,11 +7,11 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'https://vino-browsing-backend.onrender.com',
         changeOrigin: true,
       },
       '/media': {
-        target: 'http://localhost:8000',
+        target: 'https://vino-browsing-backend.onrender.com',
         changeOrigin: true,
       },
     },
